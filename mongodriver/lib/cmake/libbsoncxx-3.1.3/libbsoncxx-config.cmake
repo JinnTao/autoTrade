4222269ff769a4a6664e227300b5165ac39f4c88
JinnTao <314315805@qq.com>
2017-11-05 23:21:06 +0800

    cmake configure
    add configure

---
+++ mongodriver/lib/cmake/libbsoncxx-3.1.3/libbsoncxx-config.cmake
@@ -0,0 +1,49 @@
+# Copyright 2016 MongoDB Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set(LIBBSONCXX_VERSION_MAJOR 3)
+set(LIBBSONCXX_VERSION_MINOR 1)
+set(LIBBSONCXX_VERSION_PATCH 3)
+set(LIBBSONCXX_PACKAGE_VERSION 3.1.3)
+set(LIBBSONCXX_LIBRARIES bsoncxx)
+
+
+####### Expanded from @PACKAGE_INIT@ by configure_package_config_file() #######
+####### Any changes to this file will be overwritten by the next CMake run ####
+####### The input file was libbsoncxx-config.cmake.in                            ########
+
+get_filename_component(PACKAGE_PREFIX_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../" ABSOLUTE)
+
+macro(set_and_check _var _file)
+  set(${_var} "${_file}")
+  if(NOT EXISTS "${_file}")
+    message(FATAL_ERROR "File or directory ${_file} referenced by variable ${_var} does not exist !")
+  endif()
+endmacro()
+
+macro(check_required_components _NAME)
+  foreach(comp ${${_NAME}_FIND_COMPONENTS})
+    if(NOT ${_NAME}_${comp}_FOUND)
+      if(${_NAME}_FIND_REQUIRED_${comp})
+        set(${_NAME}_FOUND FALSE)
+      endif()
+    endif()
+  endforeach()
+endmacro()
+
+####################################################################################
+
+set_and_check(LIBBSONCXX_INCLUDE_DIRS "${PACKAGE_PREFIX_DIR}/include/bsoncxx/v_noabi")
+set_and_check(LIBBSONCXX_LIBRARY_DIRS "${PACKAGE_PREFIX_DIR}/lib")
+
