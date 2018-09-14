3dfc4451a7b5610ecc4b792178d1ca8f5ae2dfe7
JinnTao <314315805@qq.com>
2018-09-14 09:11:52 +0800

    精简

--- src/utility/global.h
+++ src/utility/global.h
@@ -5,8 +5,6 @@
 
 #include "ThostFtdcUserApiDataType.h"
 
-
-
 struct ctpConfig {
     TThostFtdcBrokerIDType   brokerId;
     TThostFtdcInvestorIDType userId;
@@ -30,39 +28,6 @@ struct mongoConfig {
     void reset() { memset(this, 0, sizeof(mongoConfig)); }
 };
 
-//struct strategyConfig {
-//    char tradeDayDir[125];
-//    char dataBaseDir[125];
-//
-//    char startDate[25];
-//    char endDate[25];
-//
-//    // dataSource
-//    char startDateTime[150];
-//    char endDateTime[150];
-//    char collectionName[20];
-//
-//    // kingKeltNer
-//    //;计算通道中值的窗口数
-//    double kkLength;  // = 11
-//    //;计算通道宽度的偏差
-//    double kkDev;  // = 1.6
-//    //;移动止损
-//    double trailingPrcnt;  // = 0.8
-//    //;每次交易的数量
-//    double fixedSize;  // = 1
-//    //;初始化数据所用的天数
-//    double initDays;  // = 10
-//
-//    // strategy base Information
-//    char inst[300];            // strategy Trading Inst
-//    char lots[300];            // strategy Trading Inst lost
-//    char timeMode[300];        // strategy Time filter mode
-//    char collectionList[500];  // strategy collection name
-//
-//    strategyConfig() { memset(this, 0, sizeof(strategyConfig)); }
-//    void reset() { memset(this, 0, sizeof(strategyConfig)); }
-//};
 
 struct sInstrumentInfo {
     char   InstrumentID[32];
